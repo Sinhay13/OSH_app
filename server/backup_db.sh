6dca6c646f7adce31b4991cef95d8fd0ab76feff
Sinhay13 <Sinhay13@gmail.com>
2024-08-17 19:09:19 +0900

    updated script

--- server/backup_db.sh
+++ server/backup_db.sh
@@ -4,11 +4,11 @@
 SOURCE_PATH="/Users/oltar/Documents/CodeLocal /Perso/Oltar/OSH_app/server/oltar.db"
 DEST_BASE_PATH="/Users/oltar/Library/CloudStorage/OneDrive-Personal/Oltar/1.Will/OSH/db"
 
-# Get the current date in YYYY-MM-DD format
-DATE=$(date +"%Y-%m-%d")
+# Get the current date and time in YYYY-MM-DD_HH-MM-SS format
+DATE_TIME=$(date +"%Y-%m-%d_%H-%M-%S")
 
-# Create a new directory with the current date
-DEST_PATH="${DEST_BASE_PATH}/${DATE}"
+# Create a new directory with the current date and time
+DEST_PATH="${DEST_BASE_PATH}/${DATE_TIME}"
 mkdir -p "${DEST_PATH}"
 
 # Copy the database file to the new directory
